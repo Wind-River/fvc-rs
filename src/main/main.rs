630a7602b188ca642c5d82b8fc5d01c3c22f92b6
Justin Yeo <justin.yeo@windriver.com>
2023-05-10 14:26:21 -0700

    extracts sub-archives with libarchive crate

--- src/main/main.rs
+++ src/main/main.rs
@@ -14,10 +14,13 @@ use walkdir::WalkDir;
 
 use std::fs::metadata;
 use std::io::Write;
+use std::path::Path;
 use clap::Parser;
 
 use std::fs::File;
 use std::path::PathBuf;
+
+use libarchive::reader::Builder;
 
 #[derive(Parser, Debug)]
 #[command(version=None)] // causes version to be read from Cargo.toml
@@ -90,22 +93,47 @@ fn calculate_fvc(cli: &CLI, hasher: &mut FVC2Hasher, files: &[PathBuf]) -> std::
         };
 
         if stat.is_file() {
-            if cli.log_verbose() {
-                eprintln!("Adding file \"{}\"", path.display());
-            }
-            // open file
-            let file = match File::open(path) {
-                Ok(file) => file,
-                Err(err) => {
-                    eprintln!("{}", err);
-                    std::process::exit(1);
+            if is_archive(path) {
+                eprintln!("TODO extract archive");
+                let tmp = match tempdir::TempDir::new("") {
+                    Ok(tmp) => tmp,
+                    Err(err) => return Err(err)
+                };
+                match extract_archive(&path, tmp.path()) {
+                    ExtractResult::Ok => {
+                        if cli.log_verbose() {
+                            eprintln!("extracted archive {}", path.display());
+                        }
+                        match calculate_fvc(&cli, hasher, &[tmp.into_path()]) {
+                            Ok(()) => (),
+                            Err(err) => {
+                                eprintln!("error processing extract archive {:#?}", err);
+                                return Err(err)
+                            }
+                        };
+                    },
+                    ExtractResult::ArchiveError(err) => {
+                        eprintln!("error reading archive {:#?}", err);
+                    }
+                };
+            } else {
+                if cli.log_verbose() {
+                    eprintln!("Adding file \"{}\"", path.display());
                 }
-            };
-            // pass open file to hasher
-            match hasher.read(file) {
-                Ok(_size) => (),
-                Err(err) => {
-                    return Err(err);
+                // open file
+                let file = match File::open(path) {
+                    Ok(file) => file,
+                    Err(err) => {
+                        eprintln!("{}", err);
+                        std::process::exit(1);
+                    }
+                };
+                // pass open file to hasher
+                match hasher.read(file) {
+                    Ok(_size) => (),
+                    Err(err) => {
+                        return Err(err);
+                    }
                 }
             }
         } else if stat.is_dir() {
@@ -149,4 +177,85 @@ fn calculate_fvc(cli: &CLI, hasher: &mut FVC2Hasher, files: &[PathBuf]) -> std::
     }
 
     return Ok(())
+}
+
+fn is_archive(path: &Path) -> bool {
+    match path.extension() {
+        None => false,
+        Some(ext) => {
+            match ext.to_str() {
+                None => false,
+                Some("zip") => true,
+                Some("gz")=> true,
+                Some("bz2") => true,
+                Some("xz") => true,
+                Some(_) => false
+            }
+        }
+    }
+}
+
+enum ExtractResult {
+    Ok,
+    // IOError(std::io::Error),
+    ArchiveError(libarchive::error::ArchiveError)
+}
+
+fn extract_archive(src: &Path, dst: &Path) -> ExtractResult {
+    let mut builder = Builder::default();
+    builder.support_format(libarchive::archive::ReadFormat::All).expect("support all formats");
+    builder.support_filter(libarchive::archive::ReadFilter::All).expect("support all filters");
+    // builder.support_format(libarchive::archive::ReadFormat::Tar).expect("support tar");
+    // builder.support_format(libarchive::archive::ReadFormat::Zip).expect("support zip");
+    // builder.support_filter(libarchive::archive::ReadFilter::Gzip).expect("support gz");
+    // // builder.support_filter(libarchive::archive::ReadFilter::Bzip2).expect("support bz2");
+    // builder.support_filter(libarchive::archive::ReadFilter::Xz).expect("suppor xz");
+
+    let mut reader = match builder.open_file(src) {
+        Ok(reader) => reader,
+        Err(err) => return ExtractResult::ArchiveError(err)
+    };
+
+    let mut opts = libarchive::archive::ExtractOptions::new();
+    opts.add(libarchive::archive::ExtractOption::Time);
+    opts.add(libarchive::archive::ExtractOption::ACL);
+    opts.add(libarchive::archive::ExtractOption::FFlags);
+
+    let writer = libarchive::writer::Disk::new();
+    match writer.set_options(&opts) {
+        Ok(()) => (),
+        Err(err) => return ExtractResult::ArchiveError(err)
+    };
+
+    match writer.write(&mut reader, dst.to_str()) {
+        Ok(_size) => ExtractResult::Ok,
+        Err(err) => ExtractResult::ArchiveError(err)
+    }
+
+    // match FileReader::open(builder, src) {
+    //     Ok(mut reader) => {
+    //         while let Some(entry) = reader.next_header() {
+    //             let target = dst.join(entry.pathname());
+    //             let mut file = match File::create(&target) {
+    //                 Ok(file) => file,
+    //                 Err(err) => return ExtractResult::IOError(err),
+    //             };
+
+    //             // entry.set_pathname(&target);
+
+    //             match reader.read_block() {
+    //                 Ok(Some(payload)) => {
+    //                     match file.write_all(payload) {
+    //                         Ok(()) => (),
+    //                         Err(err) => return ExtractResult::IOError(err),
+    //                     }
+    //                 },
+    //                 Ok(None) => (),
+    //                 Err(err) => return ExtractResult::ArchiveError(err),
+    //             };
+    //         }
+    //         ExtractResult::Ok
+    //     },
+    //     Err(err) => ExtractResult::ArchiveError(err)
+    // }
 }
